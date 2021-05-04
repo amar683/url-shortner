771c2f2526522c14fb3721ea47b359c61ab79c16
amar683 <amartomar683@gmail.com>
2021-05-05 03:10:22 +0530

    Initial Commit

---
+++ models/shortUrl.js
@@ -0,0 +1,15 @@
+const mongoose = require ('mongoose')
+const shortId = require('shortId')
+const shortUrlSchema = new mongoose.Schema({
+    full: {
+      type: String,
+      required: true
+    },
+    short: {
+      type: String,
+      required: true,
+      default : shortId.generate
+    }
+  })
+  
+  module.exports = mongoose.model('ShortUrl', shortUrlSchema)
